3619ab8e932b620444dfaa5e096261870ffd03c9
Sathiyaraman M <Sathiyaraman2003@gmail.com>
2025-11-16 10:43:53 +0530

    chore: update examples with new refinements

--- ollama-sdk/examples/basic_generate.rs
+++ ollama-sdk/examples/basic_generate.rs
@@ -1,17 +1,13 @@
-use ollama_sdk::types::generate::SimpleGenerateRequest;
-use ollama_sdk::OllamaClient;
+use ollama_sdk::{types::generate::SimpleGenerateRequest, OllamaClient};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let client = OllamaClient::builder().build()?;
 
-    let generate_request = SimpleGenerateRequest {
-        model: "llama3.2:3b".to_string(),
-        prompt: "Tell me a story about a Rust programmer."
-            .to_string()
-            .into(),
-        ..Default::default()
-    };
+    let model = "llama3.2:3b".to_string();
+    let prompt = "Tell me a story about a Rust programmer.".to_string();
+
+    let generate_request = SimpleGenerateRequest::new(model, prompt);
 
     let generate_response = client.generate_simple(generate_request).await?;
 
--- ollama-sdk/examples/streaming_chat.rs
+++ ollama-sdk/examples/streaming_chat.rs
@@ -1,25 +1,24 @@
 use futures::StreamExt;
-use ollama_sdk::types::chat::ChatRequestMessage;
-use ollama_sdk::types::chat::ChatStreamEvent;
-use ollama_sdk::types::chat::StreamingChatRequest;
-use ollama_sdk::types::Role;
-use ollama_sdk::OllamaClient;
+use ollama_sdk::{
+    types::{
+        chat::{ChatRequestMessage, ChatStreamEvent, StreamingChatRequest},
+        Role,
+    },
+    OllamaClient,
+};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let client = OllamaClient::builder().build()?;
 
-    let messages = vec![ChatRequestMessage {
-        role: Role::User,
-        content: "Tell me a story about a Rust programmer.".to_string(),
-        ..Default::default()
-    }];
+    let model = "llama3.2:3b".to_string();
 
-    let chat_request = StreamingChatRequest {
-        model: "llama3.2:3b".to_string(),
-        messages,
-        ..Default::default()
-    };
+    let messages = vec![ChatRequestMessage::new(
+        Role::User,
+        "Tell me a story about a Rust programmer.".to_string(),
+    )];
+
+    let chat_request = StreamingChatRequest::new(model, messages);
 
     let mut stream = client.chat_stream(chat_request).await?;
 
@@ -33,7 +32,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             Err(e) => eprintln!("Chat Error: {}", e),
         }
     }
-    println!();
 
     Ok(())
 }
